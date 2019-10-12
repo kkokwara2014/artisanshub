683a6352dc9fad8fcca37aec226d11db0fe15067
kkokwara2014 <kkokwara2014@gmail.com>
2019-10-12 07:10:33 +0100

    adjusting default user image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->tinyInteger('isactive')->default('1');
-            $table->text('userimage')->default('defaultimage.jpg');
+            $table->text('userimage')->default('defaultuserimage.jpg');
             $table->rememberToken();
             $table->timestamps();
         });
